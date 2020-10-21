154782223b8f5831d7f29f80af3bf478991c56ef
Ryan Lane <ryanilane@gmail.com>
2020-10-21 19:38:03 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+DISTNAME = 'clemnet'
+DESCRIPTION = 'secdetect: Convolutional neural network for predicting fluorescence from EM images'
+MAINTAINER = 'Ryan Lane'
+MAINTAINER_EMAIL = 'r.i.lane@tudelft.nl'
+LICENSE = 'LICENSE'
+README = 'README.md'
+URL = 'https://github.com/lanery/clemnet'
+VERSION = '0.1.dev'
+PACKAGES = [
+    'model',
+]
+INSTALL_REQUIRES = [
+    'tensorflow',
+]
+
+if __name__ == '__main__':
+
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        packages=PACKAGES,
+        include_package_data=True,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=open(README).read(),
+        install_requires=INSTALL_REQUIRES,
+    )
