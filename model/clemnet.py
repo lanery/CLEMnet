8bd820405134544642ea01b592c8d0027d547d39
Ryan Lane <ryanilane@gmail.com>
2020-10-21 19:26:22 +0200

    puny model

--- model/clemnet.py
+++ model/clemnet.py
@@ -1,10 +1,25 @@
 from pathlib import Path
 
-import numpy
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
-def get_model(parameter_list):
+__all__ = ['get_model']
+
+
+def get_model(input_shape):
     """
-    docstring
+    U-net-like convolutional neural network
     """
-    pass
+    inputs = layers.Input()
+
+    conv0 = layers.Conv2D(32, 3, padding='same', activation='relu')(inputs)
+    pool0 = layers.MaxPooling2D(2)(conv0)
+
+    conv1 = layers.Conv2D(64, 3, padding='same', activation='relu')(pool0)
+
+    model = keras.Model(inputs=inputs,
+                        outputs=conv1)
+    return model
