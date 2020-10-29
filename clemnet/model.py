3dbf1be816a7f2625347acce8046a8bab2272f06
lanery <ryanilane@gmail.com>
2020-10-29 14:55:42 +0100

    added unet

--- clemnet/model.py
+++ clemnet/model.py
@@ -6,22 +6,22 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
-__all__ = ['get_model']
+__all__ = ['get_model',
+           'unet']
 
 
 def get_model(input_shape=(1024, 1024)):
-    """
-    U-net-like convolutional neural network
+    """U-net-like convolutional neural network
 
     Parameters
     ----------
     input_shape : tuple
-        Image data input shape (optional)
-        Default: (1024, 1024, 1)
+        Shape of input image data
 
     Returns
     -------
     model : `keras.Model`
+        The model (duh)
     """
     inputs = layers.Input(input_shape=(*input_shape, 1))
 
@@ -49,5 +49,76 @@ def get_model(input_shape=(1024, 1024)):
     outputs = layers.Dense(1, activation='sigmoid')(x)
 
     model = keras.Model(inputs, outputs)
+
+    return model
+
+
+def unet(input_shape=(256, 256)):
+    """Convolutional network architecture for fast segmentation of images
+
+    Parameters
+    ----------
+    input_shape : tuple
+        Image data input shape (optional)
+
+    Returns
+    -------
+    model : `keras.Model`
+        The model (duh)
+
+    References
+    ----------
+    [1] U-Net: Convolutional Networks for Biomedical Image Segmentation
+        https://arxiv.org/abs/1505.04597
+    [2] Adapted from
+        https://github.com/zhixuhao/unet/blob/master/model.py
+    """
+    # Create input layer
+    input_shape = (*input_shape, 1) if len(input_shape) < 3 else input_shape
+    inputs = layers.Input(shape=input_shape)
+
+    # Downsampling arm
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = layers.MaxPooling2D(2)(conv1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = layers.MaxPooling2D(2)(conv2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = layers.MaxPooling2D(2)(conv3)
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    drop4 = layers.Dropout(0.5)(conv4)
+    pool4 = layers.MaxPooling2D(2)(drop4)
+
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    drop5 = layers.Dropout(0.5)(conv5)
+
+    # Upsampling arm
+    up6 = layers.Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(drop5))
+    merge6 = layers.concatenate([drop4, up6], axis=3)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+    up7 = layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv6))
+    merge7 = layers.concatenate([conv3, up7], axis=3)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    up8 = layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv7))
+    merge8 = layers.concatenate([conv2, up8], axis=3)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+    up9 = layers.Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv8))
+    merge9 = layers.concatenate([conv1, up9], axis=3)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = layers.Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv10 = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = keras.Model(input=inputs, output=conv10)
 
     return model
