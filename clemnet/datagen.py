4a4bbb31b8518dfeef3d22ad676de9cd8937097e
Ryan Lane <ryanilane@gmail.com>
2020-11-04 10:19:42 +0100

    adding augmentation functionality

--- clemnet/datagen.py
+++ clemnet/datagen.py
@@ -22,10 +22,11 @@ class TilePairGenerator(keras.utils.Sequence):
         List of target FM filepaths
     """
 
-    def __init__(self, batch_size, fps_src, fps_tgt):
+    def __init__(self, batch_size, fps_src, fps_tgt, augment=True):
         self.batch_size = batch_size
         self.fps_src = fps_src
         self.fps_tgt = fps_tgt
+        self.augment = augment
 
     def __len__(self):
         return len(self.fps_tgt) // self.batch_size
@@ -38,20 +39,85 @@ class TilePairGenerator(keras.utils.Sequence):
         fps_src_batch = self.fps_src[i: (i+self.batch_size)]
         fps_tgt_batch = self.fps_tgt[i: (i+self.batch_size)]
 
-        # Create batch of EM images
+        # Create batches of EM and FM images
         batch_EM = []
-        for fp in fps_src_batch:
-            image = imread(fp, as_gray=True) / 255.
-            image = image[..., np.newaxis].astype(np.float16)
-            batch_EM.append(image)
-
-        # Create batch of FM images
         batch_FM = []
-        for fp in fps_tgt_batch:
-            image = imread(fp, as_gray=True) / 255.
-            # Downscale (1024, 1024) --> (256, 256)
-            image = downscale_local_mean(image, factors=(4, 4))
-            image = image[..., np.newaxis].astype(np.float16)
-            batch_FM.append(image)
+        for fp_EM, fp_FM in zip(fps_src_batch, fps_tgt_batch):
+            image_EM, image_FM = self.fetch_image_pairs(fp_EM, fp_FM)
+            batch_EM.append(image_EM)
+            batch_FM.append(image_FM)
 
         return np.array(batch_EM), np.array(batch_FM)
+
+    def fetch_image_pairs(self, fp_EM, fp_FM):
+        """"""
+        image_EM = imread(fp_EM) / 255.
+        image_FM = imread(fp_FM) / 255.
+
+        # Apply augmentations
+        if self.augment:
+            # Augmentation functions in tf.keras.preprocessing.image
+            # require 3 channel (RGB) input images
+            image = np.stack([image_EM, image_FM], axis=2)
+            image = augment(image)
+            image_EM = image[:,:,0]
+            image_FM = image[:,:,1]
+
+        # Downscale FM (1024, 1024) --> (256, 256)
+        image_FM = downscale_local_mean(image_FM, factors=(4, 4))
+
+        # Add dummy axis to make tensorflow happy
+        # and convert to float16 to save on memory
+        image_EM = image_EM[..., np.newaxis].astype(np.float16)
+        image_FM = image_FM[..., np.newaxis].astype(np.float16)
+
+        return image_EM, image_FM
+
+
+def augment(image, flips=True, rotation=True, translation=True,
+            scale=True, contrast=True, brightness=True):
+    """Apply image augmentation
+
+    Parameters
+    ----------
+    images : list
+        Input images to be augmented
+
+    Returns
+    -------
+    images : list
+        Augmented images
+    """
+    kwargs = {'row_axis': 0,
+              'col_axis': 1,
+              'channel_axis': 2,
+              'fill_mode': 'reflect'}
+    
+    # Flips
+    if flips:
+        image = tf.image.random_flip_left_right(image).numpy()
+        image = tf.image.random_flip_up_down(image).numpy()
+
+    # Rotation
+    if rotation:
+        image = tf.keras.preprocessing.image\
+                  .random_rotation(image, rg=30, **kwargs)
+
+#     # Translation
+#     if translation:
+#         image = tf.keras.preprocessing.image\
+#                   .random_shift(image, wrg=0.2, hrg=0.2, **kwargs)
+
+#     # Scale
+#     if scale:
+#         kwargs['fill_mode'] = 'constant'
+#         image = tf.keras.preprocessing.image\
+#                   .random_zoom(image, zoom_range=(0.8, 1.2), **kwargs)
+
+    # Contrast / brightness
+    if contrast:
+        image = tf.image.random_contrast(image, 0.75, 1.5).numpy()
+    if brightness:
+        image = tf.image.random_brightness(image, 0.2).numpy()
+
+    return image
