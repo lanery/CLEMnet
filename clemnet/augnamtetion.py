88769aa669d0a052484ee07144e1c42800702903
Ryan Lane <ryanilane@gmail.com>
2021-01-19 16:45:24 +0100

    added poisson noise and elastic deformation

--- clemnet/augnamtetion.py
+++ clemnet/augnamtetion.py
@@ -10,17 +10,18 @@ __all__ = ['augment',
 
 # Default augmentations
 DEFAULT_AUGMENTATIONS = {
-    'flips': True,
-    'rotation': True,
-    'translation': False,
-    'scale': False,
-    'contrast': False,
-    'brightness': False
+    'flip': 1,
+    'rotation': 1,
+    'crop': 0.5,
+    'contrast': 0.75,
+    'brightness': 0.75,
+    'noise': 0,
+    'elastic': 0
 }
 
 
-def augment(x, y, flip=0, rotation=0, translation=0,
-            crop=0, contrast=0, brightness=0, noise=0):
+def augment(x, y, flip=0, rotation=0, translation=0, crop=0,
+            elastic=0, contrast=0, brightness=0, noise=0):
     """Apply various image augmentations
 
     Parameters
@@ -46,69 +47,84 @@ def augment(x, y, flip=0, rotation=0, translation=0,
     if flip:
         # Give a `flip`% chance of flipping in each direction
         u = tf.random.uniform([], 0, 1, dtype=tf.float32)
-        # Probabilities on probabilities since `random_flip_`
+        # Probabilities on probabilities since `random_flip_a_b`
         # already has a built-in 50/50% chance of flipping
-        xy = tf.cond(flip > u, lambda: tf.image.random_flip_left_right(xy), lambda: xy)
-        xy = tf.cond(flip > u, lambda: tf.image.random_flip_up_down(xy), lambda: xy)
-
+        xy = tf.cond(u > flip, lambda: xy, 
+                               lambda: tf.image.random_flip_left_right(xy))
+        xy = tf.cond(u > flip, lambda: xy,
+                               lambda: tf.image.random_flip_up_down(xy))
     # Rotation
     if rotation:
-        # Give a `rotation`% chance of rotating a multiple of 90degs in a random direction
+        # Give a `rotation`% chance of rotating 0, 90, 180, or 270deg
         u = tf.random.uniform([], 0, 1, dtype=tf.float32)
         d = tf.random.uniform([], 0, 4, dtype=tf.int32)
-        xy = tf.cond(rotation > u, lambda: tf.image.rot90(xy, d), lambda: xy)
-
+        xy = tf.cond(u > rotation, lambda: xy,
+                                   lambda: tf.image.rot90(xy, d))
     # Crop
     if crop:
         # Give a `crop`% chance of cropping the image 1-20%
         u = tf.random.uniform([], 0, 1, dtype=tf.float32)
-        xy = tf.cond(rotation > u, lambda: crop__(xy), lambda: xy)
+        xy = tf.cond(u > crop, lambda: xy,
+                               lambda: crop_augmentation(xy))
+    # Elastic deformation
+    if elastic:
+        # Give an `elastic`% chance of applying an elastic deformation
+        u = tf.random.uniform([], 0, 1, dtype=tf.float32)
+        flow = tf.random.normal([1, 512, 512, 2]) * (1+2*u)
+        xy_ = tf.expand_dims(xy, axis=0)
+        xy = tf.cond(u > elastic, lambda: xy,
+                                  lambda: tf.squeeze(tfa.image.dense_image_warp(xy_, flow)))
 
-#     # Contrast / brightness
-#     if contrast:
-#         xy = tf.image.random_contrast(xy, 0.75, 1.5)
-#     if brightness:
-#         xy = tf.image.random_brightness(xy, 0.2)
-#     # Noise
-#     if noise:
-#         raise NotImplementedError("noise not implemented yet.")
-# #         xy = 
-
-    # Split back into separate tensors
+    # Split back into separate tensors for remaining augmentations
+    # which are only applied to the EM images
     x, y = tf.split(xy, 2, axis=2)
+
+    # Contrast
+    if contrast:
+        # Give a `contrast`% chance of adjusting the contrast 75-150%
+        u = tf.random.uniform([], 0, 1, dtype=tf.float32)
+        x = tf.cond(u > contrast, lambda: x,
+                                  lambda: tf.image.random_contrast(x, 0.75, 1.5))
+    # Brightness
+    if brightness:
+        # Give a `brightness`% chance of adjusting the brightness +/-20%
+        u = tf.random.uniform([], 0, 1, dtype=tf.float32)
+        x = tf.cond(u > brightness, lambda: x,
+                                    lambda: tf.image.random_brightness(x, 0.2))
+    # Noise
+    if noise:
+        # Give a `noise`% chance of applying Poissonian noise
+        u = tf.random.uniform([], 0, 1, dtype=tf.float32)
+        lam = tf.random.uniform([], 4, 8, dtype=tf.int32)
+        x = tf.cond(u > noise, lambda: x,
+                               lambda: tf.random.poisson([], x*lam)/lam)
 
     return x, y
 
 
-def elastic_transform(image, alpha=400, sigma=20):
-    """Apply a randomly generated elastic tranform
+def crop_augmentation(x):
+    """Apply a random crop augmentation
 
-    Parameters
+    References
     ----------
-    image : (M, N, d) array
-        Input image to be transformed
-    alpha : scalar
-        Amplitude of Gaussian filtered noise
-    sigma : scalar
-        Smoothing factor, standard deviation for Gaussian kernel
-
-    Returns
-    -------
-    transformed : (M, N, d) array
-        Elastically transformed image    
+    [1] https://www.wouterbulten.nl/blog/tech/
+        data-augmentation-using-tensorflow-data-dataset/
     """
+    # Generate 20 crop settings, ranging from a 1% to 20% crop.
+    scales = list(np.arange(0.8, 1.0, 0.01))
+    boxes = np.zeros((len(scales), 4))
 
-    # Gaussian filter some noise
-    dx = gaussian_filter((np.random.rand(*image.shape) * 2 - 1), sigma) * alpha
-    dy = gaussian_filter((np.random.rand(*image.shape) * 2 - 1), sigma) * alpha
+    for i, scale in enumerate(scales):
+        x1 = y1 = 0.5 - (0.5 * scale)
+        x2 = y2 = 0.5 + (0.5 * scale)
+        boxes[i] = [x1, y1, x2, y2]
 
-    # Create distortion grid
-    x, y, z = np.meshgrid(np.arange(image.shape[1]),
-                          np.arange(image.shape[0]),
-                          np.arange(image.shape[2]))
-    indices = (np.reshape(y+dy, (-1, 1)),
-               np.reshape(x+dx, (-1, 1)),
-               np.reshape(z, (-1, 1)))
-    transformed = map_coordinates(image, indices, order=1, mode='reflect')
+    def random_crop(img):
+        # Create different crops for an image
+        crops = tf.image.crop_and_resize([img], boxes=boxes,
+                                         box_indices=np.zeros(len(scales)),
+                                         crop_size=(512, 512))
+        # Return a random crop
+        return crops[tf.random.uniform([], 0, len(scales), dtype=tf.int32)]
 
-    return transformed.reshape(image.shape)
+    return random_crop(x)
