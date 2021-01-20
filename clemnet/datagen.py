45f34834fe194349d29cd12c18d28ae780df9e7e
lanery <ryanilane@gmail.com>
2021-01-20 19:51:00 +0100

    smarter dataset generation

--- clemnet/datagen.py
+++ clemnet/datagen.py
@@ -41,18 +41,24 @@ def load_images(fp_src, fp_tgt):
     image_tgt = tf.io.decode_image(tf.io.read_file(fp_tgt),
                                    dtype='float32',
                                    expand_animations=False)
+
+    # Resize images to (256, 256)
+    image_src = tf.image.resize(image_src, size=[256, 256])
+    image_tgt = tf.image.resize(image_tgt, size=[256, 256])
+
     return image_src, image_tgt
 
 
-def create_dataset(fps_src, fps_tgt, batch_size=None, augment=False,
-                   augmentations=None, shuffle=False, prefetch=False):
+def create_dataset(fps_src, fps_tgt, shuffle=True, buffer_size=None,
+                   repeat=False, repetitions=None, augment=False,
+                   augmentations=None, batch=False, batch_size=None,
+                   prefetch=True):
     """Create dataset from source and target filepaths
 
     Parameters
     ----------
     fps_src : list-like
         List of source filepaths for training, validation, or testing
-
     fps_tgt : list-like
         List of target filepaths for training, validation, or testing
 
@@ -60,15 +66,38 @@ def create_dataset(fps_src, fps_tgt, batch_size=None, augment=False,
     -------
     ds : `tf.data.Dataset`
         Returns the (prefetched) `Dataset` object
+
+    References
+    ----------
+    [1] https://cs230.stanford.edu/blog/datapipeline/
     """
-    # Load images
+    # Create dataset of filepaths
     ds_fps = tf.data.Dataset.from_tensor_slices((fps_src, fps_tgt))
+
+    # Shuffle
+    if shuffle:
+        # Choose sufficiently high buffer size for proper shuffling
+        if buffer_size is None:
+            buffer_size = len(fps_src)
+        ds_fps = ds_fps.shuffle(buffer_size=buffer_size)
+
+    # Repeat
+    if repeat:
+        # Choose a reasonable(?) number of repetitions if not provided
+        if repetitions is None:
+            # TODO: choose repetitions intelligently
+            repetitions = (17-6+5)//2
+        ds_fps = ds_fps.repeat(count=repetitions)
+
+    # Load images
     ds = ds_fps.map(load_images, num_parallel_calls=AUTOTUNE)
 
-    # Apply augmentations
+    # Augment images
     if augment:
+        # Use default augmentations if not provided
         if augmentations is None:
             augmentations = DEFAULT_AUGMENTATIONS
+        # Apply image augmentations
         ds = ds.map(lambda x, y: apply_augmentations(x, y, **augmentations),
                     num_parallel_calls=AUTOTUNE)
 
@@ -76,21 +105,18 @@ def create_dataset(fps_src, fps_tgt, batch_size=None, augment=False,
     ds = ds.map(lambda x, y: (tf.clip_by_value(x, 0, 1),
                               tf.clip_by_value(y, 0, 1)))
 
-    # Resize FM
-    ds = ds.map(lambda x, y: (tf.image.resize(x, size=[256, 256]),
-                              tf.image.resize(y, size=[256, 256])),
-                num_parallel_calls=AUTOTUNE)
-
     # Convert to float16 to save on GPU RAM
     ds = ds.map(lambda x, y: (tf.image.convert_image_dtype(x, dtype='float16'),
                               tf.image.convert_image_dtype(y, dtype='float16')))
 
-    # Shuffle
-    if shuffle:
-        ds = ds.shuffle(1000)
     # Batch
-    if batch_size:
+    if batch:
+        # Choose a reasonable(?) batch size
+        if batch_size is None:
+            # TODO: choose batch size intelligently
+            batch_size = 16
         ds = ds.batch(batch_size)
+
     # Prefetch
     if prefetch:
         ds = ds.prefetch(buffer_size=AUTOTUNE)
