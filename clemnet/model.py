65a5198b6f462ec2f38c5324adf516ca1a0bdf56
lanery <ryanilane@gmail.com>
2021-01-23 19:50:16 +0100

    cleaned up models

--- clemnet/model.py
+++ clemnet/model.py
@@ -33,33 +33,33 @@ def get_model(input_shape=(1024, 1024)):
     inputs = layers.Input(shape=input_shape)
 
     # First block
-    x = layers.Conv2D(32, 3, strides=2, activation='relu', padding="same")(inputs)
+    x = layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')(inputs)
     previous_block_activation = x  # Set aside residual
 
     # Downsampling arm
     for filters in [64, 128, 256]:
         # Convolution and maxpooling
-        x = layers.Conv2D(filters, 3, activation='relu', padding="same")(x)
-        x = layers.Conv2D(filters, 3, activation='relu', padding="same")(x)
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+        x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)
+        x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)
+        x = layers.MaxPooling2D(3, strides=2, padding='same')(x)
 
         # Project residual
-        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, strides=2, padding='same')(previous_block_activation)
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
 
     # Upsampling arm
     for filters in [256, 128, 64, 32]:
         # Transpose convolution
-        x = layers.Conv2DTranspose(filters, 3, activation='relu', padding="same")(x)
-        x = layers.Conv2DTranspose(filters, 3, activation='relu', padding="same")(x)
+        x = layers.Conv2DTranspose(filters, 3, activation='relu', padding='same')(x)
+        x = layers.Conv2DTranspose(filters, 3, activation='relu', padding='same')(x)
 
         # Upsampling
         x = layers.UpSampling2D(2)(x)
 
         # Project residual
         residual = layers.UpSampling2D(2)(previous_block_activation)
-        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        residual = layers.Conv2D(filters, 1, padding='same')(residual)
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
 
@@ -96,46 +96,53 @@ def get_unet(input_shape=(256, 256)):
     input_shape = (*input_shape, 1) if len(input_shape) < 3 else input_shape
     inputs = layers.Input(shape=input_shape)
 
+    # Set up keyword arguments for convolutional layers
+    kwargs = {
+        'activation': 'relu',
+        'padding': 'same',
+        'kernel_initializer': 'he_normal'
+    }
+
     # Downsampling arm
-    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    conv1 = layers.Conv2D(64, 3, **kwargs)(inputs)
+    conv1 = layers.Conv2D(64, 3, **kwargs)(conv1)
     pool1 = layers.MaxPooling2D(2)(conv1)
-    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    conv2 = layers.Conv2D(128, 3, **kwargs)(pool1)
+    conv2 = layers.Conv2D(128, 3, **kwargs)(conv2)
     pool2 = layers.MaxPooling2D(2)(conv2)
-    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    conv3 = layers.Conv2D(256, 3, **kwargs)(pool2)
+    conv3 = layers.Conv2D(256, 3, **kwargs)(conv3)
     pool3 = layers.MaxPooling2D(2)(conv3)
-    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = layers.Conv2D(512, 3, **kwargs)(pool3)
+    conv4 = layers.Conv2D(512, 3, **kwargs)(conv4)
     drop4 = layers.Dropout(0.5)(conv4)
     pool4 = layers.MaxPooling2D(2)(drop4)
 
-    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    conv5 = layers.Conv2D(1024, 3, **kwargs)(pool4)
+    conv5 = layers.Conv2D(1024, 3, **kwargs)(conv5)
     drop5 = layers.Dropout(0.5)(conv5)
 
     # Upsampling arm
-    up6 = layers.Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(drop5))
+    up6 = layers.Conv2D(512, 2, **kwargs)(layers.UpSampling2D(2)(drop5))
     merge6 = layers.concatenate([drop4, up6], axis=3)
-    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+    conv6 = layers.Conv2D(512, 3, **kwargs)(merge6)
+    conv6 = layers.Conv2D(512, 3, **kwargs)(conv6)
 
-    up7 = layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv6))
+    up7 = layers.Conv2D(256, 2, **kwargs)(layers.UpSampling2D(2)(conv6))
     merge7 = layers.concatenate([conv3, up7], axis=3)
-    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+    conv7 = layers.Conv2D(256, 3, **kwargs)(merge7)
+    conv7 = layers.Conv2D(256, 3, **kwargs)(conv7)
 
-    up8 = layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv7))
+    up8 = layers.Conv2D(128, 2, **kwargs)(layers.UpSampling2D(2)(conv7))
     merge8 = layers.concatenate([conv2, up8], axis=3)
-    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+    conv8 = layers.Conv2D(128, 3, **kwargs)(merge8)
+    conv8 = layers.Conv2D(128, 3, **kwargs)(conv8)
 
-    up9 = layers.Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(layers.UpSampling2D(2)(conv8))
+    up9 = layers.Conv2D(64, 2, **kwargs)(layers.UpSampling2D(2)(conv8))
     merge9 = layers.concatenate([conv1, up9], axis=3)
-    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = layers.Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = layers.Conv2D(64, 3, **kwargs)(merge9)
+    conv9 = layers.Conv2D(64, 3, **kwargs)(conv9)
+    conv9 = layers.Conv2D(2, 3, **kwargs)(conv9)
     conv10 = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
 
     model = keras.Model(inputs=inputs, outputs=conv10)
@@ -150,32 +157,39 @@ def get_ogish_clemnet(input_shape=(1024, 1024)):
     input_shape = (*input_shape, 1) if len(input_shape) < 3 else input_shape
     inputs = layers.Input(shape=input_shape)
 
+    # Set up keyword arguments for convolutional layers
+    kwargs = {
+        'activation': 'relu',
+        'padding': 'same',
+        'kernel_initializer': 'he_normal'
+    }
+
     # Downsampling layers
-    conv0 = keras.layers.Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv0 = keras.layers.Conv2D(32, 3, **kwargs)(inputs)
     pool0 = keras.layers.MaxPooling2D(2)(conv0)
-    conv1 = keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool0)
+    conv1 = keras.layers.Conv2D(64, 3, **kwargs)(pool0)
     pool1 = keras.layers.MaxPooling2D(2)(conv1)
-    conv2 = keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = keras.layers.Conv2D(128, 3, **kwargs)(pool1)
     pool2 = keras.layers.MaxPooling2D(2)(conv2)
-    conv3 = keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = keras.layers.Conv2D(256, 3, **kwargs)(pool2)
     pool3 = keras.layers.MaxPooling2D(2)(conv3)
-    conv4 = keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = keras.layers.Conv2D(512, 3, **kwargs)(pool3)
     pool4 = keras.layers.MaxPooling2D(2)(conv4)
-    conv5 = keras.layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = keras.layers.Conv2D(1024, 3, **kwargs)(pool4)
     pool5 = keras.layers.MaxPooling2D(2)(conv5)
     drop5 = keras.layers.Dropout(0.5)(pool5)
 
     # Upsampling layers
-    conv6 = keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(drop5)
+    conv6 = keras.layers.Conv2D(512, 3, **kwargs)(drop5)
     up6 = keras.layers.UpSampling2D(2)(conv6)
     merge7 = keras.layers.concatenate([conv5, up6], axis=3)
-    conv7 = keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = keras.layers.Conv2D(256, 3, **kwargs)(merge7)
     up7 = keras.layers.UpSampling2D(2)(conv7)
     merge8 = keras.layers.concatenate([conv4, up7], axis=3)
-    conv8 = keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = keras.layers.Conv2D(128, 3, **kwargs)(merge8)
     up8 = keras.layers.UpSampling2D(2)(conv8)
     merge9 = keras.layers.concatenate([conv3, up8], axis=3)
-    conv9 = keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = keras.layers.Conv2D(64, 3, **kwargs)(merge9)
     up9 = keras.layers.UpSampling2D(2)(conv9)
 
     # Output layer
