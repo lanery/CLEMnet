e22b6fc83ff7f1e861cec3bfe8f16a166b12325b
lanery <ryanilane@gmail.com>
2021-01-25 18:49:39 +0100

    added function to parse tensorboard logs

--- clemnet/utils.py
+++ clemnet/utils.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 from skimage import color, exposure
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
-__all__ = ['colorize',
-           'get_n_tensors',
+__all__ = ['get_n_tensors',
            'get_max_batch_size',
+           'parse_tensorboard_logs',
+           'colorize',
            'T_HOECHST',
            'T_INSULIN',
            'T_RED',
@@ -62,6 +67,58 @@ def get_max_batch_size(gpu_ram, model):
     # Calculate max batch size
     max_batch_size = (1e9*gpu_ram / 4) / (n_tensors + model.count_params())
     return max_batch_size
+
+
+def parse_tensorboard_logs(log_dir):
+    """Parse tensorboard log directory data
+
+    Parameters
+    ----------
+    log_dir : `path.Path`
+        Log directory
+
+    Returns
+    -------
+    df : `pd.DataFrame`
+        DataFrame of tensorboard data
+    """
+    # Initialize lists for DataFrame
+    runs = []
+    datasets = []
+    tags = []
+    wall_times = []
+    steps = []
+    values = []
+
+    # Loop through log directories
+    for fp_run in log_dir.glob('[!.]*'):
+        # Either '../train' or '../validation'
+        for fp_trnval in fp_run.glob('*'):
+            fp_event = list(fp_trnval.glob('*.v2'))[0]
+            events = EventAccumulator(fp_event.as_posix()).Reload()
+            # Loop through recorded events
+            # e.g. ['epoch_loss', 'epoch_accuracy', 'epoch_pearson']
+            for tag in events.Tags()['scalars']:
+                runs.append(fp_run.name)
+                datasets.append(fp_trnval.name)
+                tags.append(tag)
+                wall_times.append([event.wall_time for event in events.Scalars(tag)])
+                steps.append([event.step for event in events.Scalars(tag)])
+                values.append([event.value for event in events.Scalars(tag)])
+
+    # Build DataFrame
+    df = pd.DataFrame({
+        'run': runs,
+        'dataset': datasets,
+        'tag': tags,
+        'wall_time': wall_times,
+        'step': steps,
+        'value': values
+    })
+    # Explode events
+    df = df.apply(lambda x: pd.Series.explode(x))\
+           .reset_index(drop=True)
+    return df
 
 
 def colorize(image, T):
