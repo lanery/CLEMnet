2a63f5306e0a5b5b47b8d07869c1e5cf36234ec0
lanery <ryanilane@gmail.com>
2021-02-05 18:09:23 +0100

    lots of new metrics

--- clemnet/metrics.py
+++ clemnet/metrics.py
@@ -1,21 +1,118 @@
 import numpy as np
 from scipy import stats
+from skimage.filters import threshold_otsu
+
 from tensorflow.python.keras import backend as K
 
 
-def pearson(X, Y):
+__all__ = ['accuracy',
+           'accuracy_tf',
+           'MCC',
+           'costes']
+
+
+def accuracy(X, Y):
+    """Mean squared error flipped for the sake of accuracy"""
+    mse = ((X - Y)**2).mean()
+    return 1-mse
+
+def accuracy_tf(X, Y):
+    """Mean squared error for tensors flipped for the sake of accuracy"""
+    mse = K.mean((X - Y)**2)
+    return 1-mse
+
+
+def PCC(X, Y, thresholding=None):
     """Pearson correlation coefficient"""
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate Pearson correlation coefficient
+    r = stats.pearsonr(X.ravel(), Y.ravel())[0]
+    return r
+
+def PCC_tf(X, Y):
+    """Pearson correlation coefficient for tensors"""
     x0 = X - K.mean(X)
     y0 = Y - K.mean(Y)
     r = K.sum(x0*y0) / K.sqrt(K.sum(x0**2) * K.sum(y0**2))
     return r
 
 
-# def acc_pred(y_true, y_pred):
-#     'accuracy metric as described in progress report'
-#     a = (1-abs(y_true-y_pred))
-#     b = K.sum(a)/(256**2)/batch_size
-#     return b
+def MCC(X, Y, thresholding='costes'):
+    """Manders colocalization coefficients"""
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate colocalization coefficients
+    Xco = np.where(Y > 0, X, 0)
+    Yco = np.where(X > 0, Y, 0)
+    M1 = Xco.sum() / X.sum()
+    M2 = Yco.sum() / Y.sum()
+    return 2*(M1-0.5), 2*(M2-0.5)
+
+
+def ICQ(X, Y, thresholding=None):
+    """Intensity correlation quotient"""
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate intensity quotient
+    x0 = X - X.mean()
+    y0 = Y - Y.mean()
+    q = (x0*y0 > 0).mean()
+    return 2*(q-0.5)
+
+def ICQ_tf(X, Y):
+    """Intensity correlation quotient for tensors"""
+    x0 = X - K.mean(X)
+    y0 = Y - K.mean(Y)
+    q = K.mean(x0*y0 > 0)
+    return 2*(q-0.5)
+
+
+def spearman(X, Y, thresholding=None):
+    """Spearman correlation coefficient"""
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate Pearson correlation coefficient
+    r = stats.spearmanr(X.ravel(), Y.ravel())[0]
+    return r
+
+
+def overlap(X, Y, thresholding=None):
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate overlap coefficient
+    o = (X*Y).sum() / np.sqrt((X**2).sum() * (Y**2).sum())
+    return o
+
+
+def intersection(X, Y, thresholding=None):
+    # Apply thresholding
+    X, Y = threshold(X, Y, method=thresholding)
+    # Calculate intersection coefficient
+    i = (X*Y).sum() / (X.sum() + Y.sum() - (X*Y).sum())
+    return i
+
+
+def threshold(X, Y, method=None, c_val=0):
+    """Threshold images"""
+    # Determine threshold values
+    if method == 'costes':
+        Tx, Ty = costes(X, Y)
+    elif method == 'otsu':
+        Tx = threshold_otsu(X)
+        Ty = threshold_otsu(Y)
+    elif method == 'constant':
+        # Allow for independent threshold values
+        if hasattr(c_val, '__len__'):
+            Tx, Ty = c_val
+        else:
+            Tx, Ty = c_val, c_val
+    else:  # no thresholding
+        return X, Y
+    # Apply threshold
+    X = np.where(X > Tx, X, 0)
+    Y = np.where(Y > Ty, Y, 0)
+    return X, Y
 
 
 def costes(X, Y, r_min=0.01):
@@ -26,10 +123,19 @@ def costes(X, Y, r_min=0.01):
 
     Parameters
     ----------
-    X : 
+    X : (N,) or (M, N) array
+        Input array for signal 1
+    Y : (N,) or (M, N) array
+        Input array for signal 2
+    r_min : float, optional
+        Threshold Pearson correlation coefficient
 
     Returns
     -------
+    Tx : scalar
+        Background threshold for signal 1
+    Ty : scalar
+        Background threshold for signal 2
 
     References
     ----------
